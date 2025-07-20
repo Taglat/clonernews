a83daa49591fb05751ef42bb772258b359aebae3
talgat <ttokayev7@gmail.com>
2025-07-20 20:40:37 +0600

    Сделал эмуляцию загрузки первых десяти постов на страницах New Stories, Top Stories, Jobs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-import {getNewStories, getItemById } from "./api.js";
+import {getNewStories, getTopStories, getJobs, getItemById } from "./api.js";
+
+const currentPagePath = window.location.pathname;
+
+let ids = [];
+
+if (currentPagePath.endsWith("index.html")) {
+    ids = await getNewStories();
+} else if (currentPagePath.endsWith("top-stories.html")) {
+    ids = await getTopStories();
+} else if (currentPagePath.endsWith("jobs.html")) {
+    ids = await getJobs();
+}
 
 const page = 0;
 const perPage = 10;
 
-const ids = await getNewStories();
-
 const currentPageIds = ids.slice(page * perPage, (page + 1) * perPage);
-const posts = await Promise.all(currentPageIds.map(id => getItemById(id)));
+const items = await Promise.all(currentPageIds.map(id => getItemById(id)));
+
+console.log(currentPagePath, items)
